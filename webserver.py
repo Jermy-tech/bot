1cb8d407f230ffc291ca0a917eb025a6ed012c36
Jermy-tech <penajermy24@gmail.com>
2024-09-25 22:44:21 -0400

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from threading import Thread
-import os
 
-app = Flask('')
+app = Flask(__name__)
+
 @app.route('/')
 def home():
     return "Online"
@@ -12,4 +12,4 @@ def run():
 
 def keep_alive():
     t = Thread(target=run)
-    t.start
+    t.start()  # Call start() method to start the thread
